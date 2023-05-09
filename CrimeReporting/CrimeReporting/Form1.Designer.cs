ed36a2c61ef95b5e8accb550d70c7390d67a4d02
Quouou <benedicto.christopheeer@gmail.com>
2023-05-09 13:22:25 +0800

    added dashboard for desktop version

---
+++ CrimeReporting/CrimeReporting/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CrimeReporting
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.navBar = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asdToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.navBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // navBar
+            // 
+            this.navBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(49)))), ((int)(((byte)(105)))));
+            this.navBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.navBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem});
+            this.navBar.Location = new System.Drawing.Point(0, 0);
+            this.navBar.Name = "navBar";
+            this.navBar.Size = new System.Drawing.Size(126, 647);
+            this.navBar.TabIndex = 0;
+            this.navBar.Text = "navBar";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asdToolStripMenuItem});
+            this.homeToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(113, 19);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // asdToolStripMenuItem
+            // 
+            this.asdToolStripMenuItem.Name = "asdToolStripMenuItem";
+            this.asdToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.asdToolStripMenuItem.Text = "asd";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 647);
+            this.Controls.Add(this.navBar);
+            this.MainMenuStrip = this.navBar;
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.navBar.ResumeLayout(false);
+            this.navBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip navBar;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asdToolStripMenuItem;
+    }
+}
+
